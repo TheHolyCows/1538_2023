d24a4d8eb6343e1ed363e8099d81d9fda6dfd60c
Scott Semtner <sjsemtner@gmail.com>
2023-01-12 19:04:26 -0800

    added max speed multiplicand in controller

--- src/main/cpp/Controllers/OperatorController.cpp
+++ src/main/cpp/Controllers/OperatorController.cpp
@@ -11,10 +11,11 @@ OperatorController::OperatorController(CowControlBoard *controlboard)
 void OperatorController::Handle(CowRobot *bot)
 {
     // Swerve controls for xbox controller
-    auto chassisSpeeds = CowLib::CowChassisSpeeds::FromFieldRelativeSpeeds(m_CB->GetLeftDriveStickAxis(0),
-                                                                           -m_CB->GetLeftDriveStickAxis(1),
-                                                                           m_CB->GetLeftDriveStickAxis(4),
-                                                                           bot->GetGyro()->GetYaw());
+    auto chassisSpeeds = CowLib::CowChassisSpeeds::FromFieldRelativeSpeeds(
+        m_CB->GetLeftDriveStickAxis(0) * CONSTANT("SWERVE_MAX_SPEED"),
+        -m_CB->GetLeftDriveStickAxis(1) * CONSTANT("SWERVE_MAX_SPEED"),
+        m_CB->GetLeftDriveStickAxis(4),
+        bot->GetGyro()->GetYaw());
 
     bot->GetDrivetrain()->SetVelocity(chassisSpeeds);
 }
