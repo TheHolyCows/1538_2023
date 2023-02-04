c465a37d6e66c7d9e032d974a1841fabad1c1fdd
Ryan B <ryanburila@gmail.com>
2023-02-04 12:22:06 -0800

    Create Interface and Mock for Arm
    Added Abstract interface ArmInterface and a Mocked implementation for testing ArmMock

---
+++ src/main/cpp/Subsystems/ArmInterface.cpp
@@ -0,0 +1,68 @@
+//==================================================
+// Copyright (C) 2023 Team 1538 / The Holy Cows
+// Arm.h
+// author: Cole/JT/Constantine
+// created on: 2023-1-14
+//==================================================
+
+#include "ArmInterface.h"
+
+#include <cmath>
+#include <sstream>
+#include <stdexcept>
+
+void ArmInterface::SetAngle(double angle)
+{
+    SetSafeAngle(angle);
+    SetArmAngle(m_CurrentConfig.angle);
+    SetArmPosition(m_CurrentConfig.pos);
+}
+
+void ArmInterface::SetTelescopePosition(double position)
+{
+    SetSafePos(position);
+    SetArmPosition(m_CurrentConfig.pos);
+}
+
+void ArmInterface::SetSafeAngle(const double angle)
+{
+    // If Arm Angle is greater >= Max
+    if (angle >= m_MaxAngle)
+    {
+        // Set the current angle to MaxAngle
+        m_CurrentConfig.angle = m_MaxAngle;
+    }
+    // If Arm Angle is greater <= Max
+    else if (angle <= m_MinAngle)
+    {
+        // Set the current angle to MinAngle and position to MaxPosAtMinAngle
+        m_CurrentConfig.angle = m_MinAngle;
+    }
+    else
+    {
+        m_CurrentConfig.angle = angle;
+    }
+
+    // Check if the currenet Pos is safe at the new angle
+    SetSafePos(m_CurrentConfig.pos);
+}
+
+void ArmInterface::SetSafePos(const double position)
+{
+    // Throw is angle is invalid as this could damage bot!
+    if (m_CurrentConfig.angle > m_MaxAngle || m_CurrentConfig.angle < m_MinAngle)
+    {
+        std::stringstream ss;
+        ss << "Error: Angle is Invalid: " << m_CurrentConfig.angle << "!";
+        throw std::runtime_error(ss.str());
+    }
+    const double MaxPosAtCurrentAngle = m_ArmHeight / (std::cos(m_CurrentConfig.angle * M_PI / 180.0));
+    if (position > MaxPosAtCurrentAngle)
+    {
+        m_CurrentConfig.pos = MaxPosAtCurrentAngle;
+    }
+    else
+    {
+        m_CurrentConfig.pos = position;
+    }
+}
