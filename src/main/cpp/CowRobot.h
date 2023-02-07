950d8521796b52aeca663d7bd03c45405e997411
Ryan B <ryanburila@gmail.com>
2023-02-06 19:18:55 -0800

    Add Pivot and Telescope Interfaces and implementations

--- src/main/cpp/CowRobot.h
+++ src/main/cpp/CowRobot.h
@@ -16,7 +16,7 @@
 #include "CowPigeon.h"
 #include "Drivetrain/SwerveDrive.h"
 #include "frc/controller/PIDController.h"
-#include "Subsystems/Arm.h"
+#include "Subsystems/Arm/Arm.h"
 #include "Subsystems/Limelight.h"
 
 #include <frc/BuiltInAccelerometer.h>
