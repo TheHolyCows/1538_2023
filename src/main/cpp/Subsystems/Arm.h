47d56c71a99721352ec476e53f3c65e25d6faed5
Jon <jon-bassi@users.noreply.github.com>
2023-02-16 17:03:15 -0800

    split out state enums for Arm

--- src/main/cpp/Subsystems/Arm.h
+++ src/main/cpp/Subsystems/Arm.h
@@ -22,26 +22,6 @@
 
 class Arm : public ArmInterface
 {
-public:
-    enum ARM_STATE
-    {
-        ARM_NONE = 0,
-        ARM_IN,
-        ARM_STOW,
-        ARM_L3,
-        ARM_L2,
-        ARM_L1,
-        ARM_SCORE,
-        ARM_MANUAL
-    };
-
-    enum ARM_CARGO
-    {
-        ST_NONE = 0,
-        ST_CONE,
-        ST_CUBE
-    };
-
 private:
     /**
      * @brief Will rotate the arm to the specified angle
