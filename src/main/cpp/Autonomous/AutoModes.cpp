b6ad5bc70eb51c5f69f3a59b3e9b9cf2f5b22572
Scott Semtner <sjsemtner@gmail.com>
2023-03-20 17:59:41 -0700

    autos work

--- src/main/cpp/Autonomous/AutoModes.cpp
+++ src/main/cpp/Autonomous/AutoModes.cpp
@@ -33,72 +33,81 @@ AutoModes::AutoModes()
                                      new SeriesCommand(series) });
     };
 
-    auto startGroundIntake = [](ARM_CARGO cargo) {
+    auto setClaw = [](ARM_CARGO type)
+    {
+        return new LambdaCommand(
+            [type](CowRobot *bot)
+            {
+                bot->GetArm()->SetClawState(CLAW_INTAKE);
+                bot->GetArm()->SetArmCargo(type);
+                Vision::GetInstance()->SetCargo(type);
+                bot->GetArm()->UpdateClawState();
+                bot->GetArm()->SetClawState(CLAW_OFF);
+                bot->GetArm()->UpdateClawState();
+            });
+    };
+
+    auto startGroundIntake = [setClaw](ARM_CARGO cargo)
+    {
         return new ParallelCommand(
-            { new UpdateArmStateCommand(ARM_GND, cargo, false), new ClawCommand(CLAW_INTAKE, 0) });
+            { setClaw(cargo), new UpdateArmStateCommand(ARM_GND, cargo, false), new ClawCommand(CLAW_INTAKE, 0) });
     };
 
     auto stow = []() {
         return new SeriesCommand({ new UpdateArmStateCommand(ARM_STOW, false), new ClawCommand(CLAW_OFF, 0) });
     };
 
-    m_Modes["3 GP LZ"].push_back(new LambdaCommand(
-        [](CowRobot *bot)
-        {
-            bot->GetArm()->SetClawState(CLAW_INTAKE);
-            bot->GetArm()->SetArmCargo(CG_CONE);
-            Vision::GetInstance()->SetCargo(CG_CONE);
-            bot->GetArm()->UpdateClawState();
-            bot->GetArm()->SetClawState(CLAW_OFF);
-            bot->GetArm()->UpdateClawState();
-        }));
-    m_Modes["3 GP LZ"].push_back(new PathplannerSwerveTrajectoryCommand("cone L2 start", 8, 4, true, true));
-    m_Modes["3 GP LZ"].push_back(new UpdateArmStateCommand(ARM_L2, CG_CONE, true, true));
-    m_Modes["3 GP LZ"].push_back(new WaitCommand(0.6, false));
-    m_Modes["3 GP LZ"].push_back(new ClawCommand(CLAW_EXHAUST, 0.2));
-    m_Modes["3 GP LZ"].push_back(new UpdateArmStateCommand(ARM_DRIVER_STOW, false));
-    m_Modes["3 GP LZ"].push_back(new WaitCommand(0.1, false));
-    m_Modes["3 GP LZ"].push_back(pathWithEvents("3 GP LZ - intake cube 1",
-                                                { { 0.02, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, false) },
-                                                  { 0.3, new ClawCommand(CLAW_INTAKE, 0) },
-                                                  { 0.01, new UpdateArmStateCommand(ARM_GND, CG_CUBE, false) } },
-                                                true,
-                                                16.5,
-                                                12));
-    m_Modes["3 GP LZ"].push_back(stow());
-    m_Modes["3 GP LZ"].push_back(pathWithEvents("3 GP LZ - score cube 1",
-                                                { { 0.2, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, true) },
-                                                  { 0.1, new UpdateArmStateCommand(ARM_L2, CG_CUBE, false, true) } },
-                                                false,
-                                                16.5,
-                                                10));
-    m_Modes["3 GP LZ"].push_back(new WaitCommand(0.2, false));
-    m_Modes["3 GP LZ"].push_back(new ClawCommand(CLAW_EXHAUST, 0.2));
-    m_Modes["3 GP LZ"].push_back(new UpdateArmStateCommand(ARM_DRIVER_STOW, false));
-    m_Modes["3 GP LZ"].push_back(new WaitCommand(0.1, false));
-    m_Modes["3 GP LZ"].push_back(pathWithEvents(
+    auto scoreConeL2 = []()
+    {
+        return new SeriesCommand({ new UpdateArmStateCommand(ARM_L2, CG_CONE, true, true),
+                                   new WaitCommand(0.6, false),
+                                   new ClawCommand(CLAW_EXHAUST, 0.2),
+                                   new UpdateArmStateCommand(ARM_DRIVER_STOW, false),
+                                   new WaitCommand(0.1, false) });
+    };
+
+    std::deque<RobotCommand *> threeGPLZ;
+
+    threeGPLZ.push_back(setClaw(CG_CONE));
+    threeGPLZ.push_back(scoreConeL2());
+    threeGPLZ.push_back(pathWithEvents("3 GP LZ - intake cube 1",
+                                       { { 0.02, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, false) },
+                                         { 0.3, new ClawCommand(CLAW_INTAKE, 0) },
+                                         { 0.01, new UpdateArmStateCommand(ARM_GND, CG_CUBE, false) } },
+                                       true,
+                                       16.5,
+                                       12));
+    threeGPLZ.push_back(stow());
+    threeGPLZ.push_back(pathWithEvents("3 GP LZ - score cube 1",
+                                       { { 0.2, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, true) },
+                                         { 0.1, new UpdateArmStateCommand(ARM_L2, CG_CUBE, false, true) } },
+                                       false,
+                                       16.5,
+                                       10));
+    threeGPLZ.push_back(new WaitCommand(0.2, false));
+    threeGPLZ.push_back(new ClawCommand(CLAW_EXHAUST, 0.2));
+    threeGPLZ.push_back(new UpdateArmStateCommand(ARM_DRIVER_STOW, false));
+    threeGPLZ.push_back(new WaitCommand(0.1, false));
+    threeGPLZ.push_back(pathWithEvents(
         "3 GP LZ - intake cube 2",
         { { 0.5, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, false) }, { 1, startGroundIntake(CG_CUBE) } },
         false,
         16.5,
         8));
-    m_Modes["3 GP LZ"].push_back(stow());
-    m_Modes["3 GP LZ"].push_back(pathWithEvents("3 GP LZ - score cube 2",
-                                                { { 1, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, true) } },
-                                                false));
-    m_Modes["3 GP LZ"].push_back(new UpdateArmStateCommand(ARM_GND, true));
-    m_Modes["3 GP LZ"].push_back(new ClawCommand(CLAW_EXHAUST, 1));
+    threeGPLZ.push_back(stow());
+    threeGPLZ.push_back(pathWithEvents("3 GP LZ - score cube 2",
+                                       { { 1, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, true) } },
+                                       false));
+    threeGPLZ.push_back(new UpdateArmStateCommand(ARM_GND, true));
+    threeGPLZ.push_back(new ClawCommand(CLAW_EXHAUST, 1));
 
-    m_Modes["1 Cone Balance Mid"].push_back(new LambdaCommand(
-        [](CowRobot *bot)
-        {
-            bot->GetArm()->SetClawState(CLAW_INTAKE);
-            bot->GetArm()->SetArmCargo(CG_CONE);
-            Vision::GetInstance()->SetCargo(CG_CONE);
-            bot->GetArm()->UpdateClawState();
-            bot->GetArm()->SetClawState(CLAW_OFF);
-            bot->GetArm()->UpdateClawState();
-        }));
+    m_Modes["3 GP LZ"] = threeGPLZ;
+
+    m_Modes["3 GP & Balance LZ"] = threeGPLZ;
+    m_Modes["3 GP & Balance LZ"].push_back(new PathplannerSwerveTrajectoryCommand("LZ - to CS inside", 8, 4, true, false));
+    m_Modes["3 GP & Balance LZ"].push_back(new BalanceCommand(3, 7, 15, true));
+
+    m_Modes["1 Cone Balance Mid"].push_back(setClaw(CG_CONE));
     m_Modes["1 Cone Balance Mid"].push_back(new UpdateArmStateCommand(ARM_L3, CG_CONE, true, true));
     m_Modes["1 Cone Balance Mid"].push_back(new WaitCommand(0.8, false));
     m_Modes["1 Cone Balance Mid"].push_back(new ClawCommand(CLAW_EXHAUST, 0.2));
@@ -107,46 +116,43 @@ AutoModes::AutoModes()
     m_Modes["1 Cone Balance Mid"].push_back(new PathplannerSwerveTrajectoryCommand("to CS mid", 16.5, 6, true, true));
     m_Modes["1 Cone Balance Mid"].push_back(new BalanceCommand(3, 7, 15, true));
 
-    m_Modes["2.5 GP Guard"].push_back(new LambdaCommand(
-        [](CowRobot *bot)
-        {
-            bot->GetArm()->SetClawState(CLAW_INTAKE);
-            bot->GetArm()->SetArmCargo(CG_CONE);
-            Vision::GetInstance()->SetCargo(CG_CONE);
-            bot->GetArm()->UpdateClawState();
-            bot->GetArm()->SetClawState(CLAW_OFF);
-            bot->GetArm()->UpdateClawState();
-        }));
-    m_Modes["2.5 GP Guard"].push_back(new UpdateArmStateCommand(ARM_L2, CG_CONE, false, true));
-    m_Modes["2.5 GP Guard"].push_back(new PathplannerSwerveTrajectoryCommand("cone L2 start", 8, 4, true, true));
-    // m_Modes["2.5 GP Guard"].push_back(new LambdaCommand(
-    //     [](CowRobot *bot) {
-    //     }));
-    m_Modes["2.5 GP Guard"].push_back(new WaitCommand(0.6, false));
-    m_Modes["2.5 GP Guard"].push_back(new ClawCommand(CLAW_EXHAUST, 0.2));
-    m_Modes["2.5 GP Guard"].push_back(new UpdateArmStateCommand(ARM_DRIVER_STOW, false));
-    m_Modes["2.5 GP Guard"].push_back(new WaitCommand(0.1, false));
-    m_Modes["2.5 GP Guard"].push_back(
-        pathWithEvents("Guard - intake 1",
-                       { { 0.02, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, false) },
-                         { 0.3, new ClawCommand(CLAW_INTAKE, 0) },
-                         { 0.01, new UpdateArmStateCommand(ARM_GND, CG_CUBE, false) } },
-                       true,
-                       16.5,
-                       8));
+    std::deque<RobotCommand *> twoGPGuard;
 
-    m_Modes["2.5 GP Guard"].push_back(stow());
+    twoGPGuard.push_back(setClaw(CG_CONE));
+    twoGPGuard.push_back(new UpdateArmStateCommand(ARM_L2, CG_CONE, false, true));
+    twoGPGuard.push_back(new PathplannerSwerveTrajectoryCommand("Guard - start", 8, 4, true, true));
+    twoGPGuard.push_back(new WaitCommand(0.6, false));
+    twoGPGuard.push_back(new ClawCommand(CLAW_EXHAUST, 0.2));
+    twoGPGuard.push_back(new UpdateArmStateCommand(ARM_DRIVER_STOW, false));
+    twoGPGuard.push_back(new WaitCommand(0.1, false));
+    twoGPGuard.push_back(pathWithEvents("Guard - intake 1",
+                                        { { 0.02, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, false) },
+                                          { 0.3, new ClawCommand(CLAW_INTAKE, 0) },
+                                          { 0.01, new UpdateArmStateCommand(ARM_GND, CG_CUBE, false) } },
+                                        true,
+                                        16.5,
+                                        8));
 
-    m_Modes["2.5 GP Guard"].push_back(pathWithEvents("Guard - score 1",
-                                                   { { 0.2, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, true) },
-                                                     { 0.1, new UpdateArmStateCommand(ARM_L2, CG_CUBE, false, true) } },
-                                                   false,
-                                                   16.5,
-                                                   8));
-    m_Modes["2.5 GP Guard"].push_back(new WaitCommand(0.2, false));
-    m_Modes["2.5 GP Guard"].push_back(new ClawCommand(CLAW_EXHAUST, 0.2));
-    m_Modes["2.5 GP Guard"].push_back(new UpdateArmStateCommand(ARM_DRIVER_STOW, false));
-    m_Modes["2.5 GP Guard"].push_back(new WaitCommand(0.2, false));
+    twoGPGuard.push_back(stow());
+
+    twoGPGuard.push_back(pathWithEvents("Guard - score 1",
+                                        { { 0.2, new UpdateArmStateCommand(ARM_STOW, CG_CUBE, false, true) },
+                                          { 0.1, new UpdateArmStateCommand(ARM_L2, CG_CUBE, false, true) } },
+                                        false,
+                                        16.5,
+                                        8));
+    twoGPGuard.push_back(new WaitCommand(0.2, false));
+    twoGPGuard.push_back(new ClawCommand(CLAW_EXHAUST, 0.2));
+    twoGPGuard.push_back(new UpdateArmStateCommand(ARM_DRIVER_STOW, false));
+    twoGPGuard.push_back(new WaitCommand(0.2, false));
+    twoGPGuard.push_back(new UpdateArmStateCommand(ARM_STOW, false));
+
+    m_Modes["2 GP & Balance Guard"] = twoGPGuard;
+    m_Modes["2 GP & Balance Guard"].push_back(
+        new PathplannerSwerveTrajectoryCommand("Guard - to CS inside", 8, 4, true, false));
+    m_Modes["2 GP & Balance Guard"].push_back(new BalanceCommand(3, 7, 15, true));
+
+    m_Modes["2.5 GP Guard"] = twoGPGuard;
     m_Modes["2.5 GP Guard"].push_back(new LambdaCommand(
         [](CowRobot *bot)
         {
