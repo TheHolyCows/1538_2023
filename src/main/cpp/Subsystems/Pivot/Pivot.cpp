89bf40eb98357a7e6bee79b8d39dfa101713a83c
Jon <jon-bassi@users.noreply.github.com>
2023-04-08 15:18:10 -0700

    zero arm on hard stop now

--- src/main/cpp/Subsystems/Pivot/Pivot.cpp
+++ src/main/cpp/Subsystems/Pivot/Pivot.cpp
@@ -65,7 +65,8 @@ void Pivot::ResetConstants()
 
 void Pivot::Handle()
 {
-    m_PivotMotor->Set(ctre::phoenix::motorcontrol::ControlMode::MotionMagic, m_TargetAngle * 2048);
+    m_PivotMotor->Set(ctre::phoenix::motorcontrol::ControlMode::MotionMagic,
+                      m_TargetAngle * 2048 + CONSTANT("ARM_ZERO_OFFSET"));
 
     if (m_TickCount++ % 10 == 0) // 200 miliseconds
     {
